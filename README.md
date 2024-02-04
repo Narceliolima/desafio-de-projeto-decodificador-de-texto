df1d0aa3908a0818b784f0c28622c5bccc361820
Narcelio <narcelio.slima@gmail.com>
2024-02-04 06:05:04 -0300

    Salvando algumas referencias, implementando descriptografarTexto e melhorando o criptografaTexto.

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 ## 🖇Referência
 
  - [Teste]()
-
+ https://www.w3schools.com/jsref/jsref_string_replaceall.asp
+ https://bobbyhadz.com/blog/typescript-get-enum-key-by-value#get-an-enum-key-by-value-in-typescript
+ https://www.sohamkamani.com/javascript/enums/
--- assets/script/engine.js
+++ assets/script/engine.js
@@ -1,58 +1,58 @@
 const listaCaracteresCodificados = {a:"ai", e:"enter" , i:"imes" ,o:"ober" ,u:"ufat"}
 const listaBotoes = document.querySelectorAll("button");
 
-function criptografaTexto(){
-    const areaDeTextoDescodificado = document.querySelector("#entrada-texto");
-    const textoDescodificado = areaDeTextoDescodificado.value;
-    let textoCodificado = "";
-
-    areaDeTextoDescodificado.value = "";
-
-    for(let i=0; i<textoDescodificado.length; i++){
-        if(textoDescodificado[i]==="a"||textoDescodificado[i]==="e"||textoDescodificado[i]==="i"||textoDescodificado[i]==="o"||textoDescodificado[i]==="u"){
-            textoCodificado += listaCaracteresCodificados[textoDescodificado[i]];
-        }
-        else{
-            textoCodificado += textoDescodificado[i];
-        }
-    }
-
+function imprimeTexto(texto){
     document.querySelector(".interface-decodificador-imprimir-sem-mensagens").style.display = "none";
-
-    document.querySelector("#saida-texto").value += textoCodificado;    
+    document.querySelector("#saida-texto").value = texto;
 }
 
-function descriptografaTexto(){/*
-    const areaDeTextoCodificado = document.querySelector("#entrada-texto");
-    const textoCodificado = areaDeTextoCodificado.value;
-    let textoDescodificado = "";
+function criptografaTexto(texto){
 
-    areaDeTextoCodificado.value = "";
+    const textoCriptografado = texto.replaceAll(/a|e|i|o|u/gi, (texto) => {
+        return listaCaracteresCodificados[texto];
+    });
 
-    for(let i=0; i<textoCodificado.length; i++){
-        if(textoCodificado[i]==="ai"||textoCodificado[i]==="enter"||textoCodificado[i]==="imes"||textoCodificado[i]==="ober"||textoCodificado[i]==="ufat"){
-            textoDescodificado += listaCaracteresCodificados[textoCodificado[i]];
-        }
-        else{
-            textoDescodificado += textoCodificado[i];
-        }
-    }*/
+    return textoCriptografado;
+}
+
+function descriptografaTexto(texto){
+
+    const textoDescriptografado = texto.replaceAll(/ai|enter|imes|ober|ufat/gi, (texto) => {
+        const keyIndex = Object.values(listaCaracteresCodificados).indexOf(texto);
+        return Object.keys(listaCaracteresCodificados)[keyIndex];
+    });
+
+    return textoDescriptografado;
 }
 
 function copiaTexto(){
     console.log("Copia o texto");
 }
 
+function acionaCriDecri(acao){
+    const areaTextoEntrada = document.querySelector("#entrada-texto");
+    const texto = areaTextoEntrada.value;
+    let textoModificado = "";
+
+    areaTextoEntrada.value = "";
+
+    if(acao.srcElement.textContent==="Criptografar"){
+        textoModificado = criptografaTexto(texto);
+    }
+    else if(acao.srcElement.textContent==="Descriptografar"){
+        textoModificado = descriptografaTexto(texto);
+    }
+
+    imprimeTexto(textoModificado);
+}
+
 function iniciaBotao(botao){
 
-    if(botao.textContent==="Criptografar"){
-        botao.addEventListener("click", criptografaTexto);
-    }
-    else if(botao.textContent==="Descriptografar"){
-        botao.addEventListener("click", descriptografaTexto);
-    }
-    else if(botao.textContent==="Copiar"){
+    if(botao.textContent==="Copiar"){
         botao.addEventListener("click", copiaTexto);
+    }
+    else{
+        botao.addEventListener("click", acionaCriDecri);
     }
 }
 
