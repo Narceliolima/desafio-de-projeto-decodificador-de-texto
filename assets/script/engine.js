670fa3b2cdee5f9b2bbbaf461c9c674aecc3a11e
Narcelio <narcelio.slima@gmail.com>
2024-02-04 04:08:53 -0300

    Renomenado os textarea e implementando a função de criptografar texto

--- assets/script/engine.js
+++ assets/script/engine.js
@@ -1,11 +1,42 @@
+const listaCaracteresCodificados = {a:"ai", e:"enter" , i:"imes" ,o:"ober" ,u:"ufat"}
 const listaBotoes = document.querySelectorAll("button");
 
 function criptografaTexto(){
-    console.log("Criptografa o texto");
+    const areaDeTextoDescodificado = document.querySelector("#entrada-texto");
+    const textoDescodificado = areaDeTextoDescodificado.value;
+    let textoCodificado = "";
+
+    areaDeTextoDescodificado.value = "";
+
+    for(let i=0; i<textoDescodificado.length; i++){
+        if(textoDescodificado[i]==="a"||textoDescodificado[i]==="e"||textoDescodificado[i]==="i"||textoDescodificado[i]==="o"||textoDescodificado[i]==="u"){
+            textoCodificado += listaCaracteresCodificados[textoDescodificado[i]];
+        }
+        else{
+            textoCodificado += textoDescodificado[i];
+        }
+    }
+
+    document.querySelector(".interface-decodificador-imprimir-sem-mensagens").style.display = "none";
+
+    document.querySelector("#saida-texto").value += textoCodificado;    
 }
 
-function descriptografaTexto(){
-    console.log("Descriptografa o texto");
+function descriptografaTexto(){/*
+    const areaDeTextoCodificado = document.querySelector("#entrada-texto");
+    const textoCodificado = areaDeTextoCodificado.value;
+    let textoDescodificado = "";
+
+    areaDeTextoCodificado.value = "";
+
+    for(let i=0; i<textoCodificado.length; i++){
+        if(textoCodificado[i]==="ai"||textoCodificado[i]==="enter"||textoCodificado[i]==="imes"||textoCodificado[i]==="ober"||textoCodificado[i]==="ufat"){
+            textoDescodificado += listaCaracteresCodificados[textoCodificado[i]];
+        }
+        else{
+            textoDescodificado += textoCodificado[i];
+        }
+    }*/
 }
 
 function copiaTexto(){
